d7b6710df78a93440ed38df6500217ff61cf4873
vboxuser <vboxuser@ubu.myguest.virtualbox.org>
2025-02-16 12:13:35 +0100

    config

--- config/dev.exs
+++ config/dev.exs
@@ -1,10 +1,10 @@
 import Config
 
 config :ping, Ping.Repo,
-	username: "my_user",
-	password: "my_password",
-	hostname: "localhost",
-	database: "ping",
+	username: System.get_env("PGUSER"),
+	password: System.get_env("PGPASSWORD"),
+	hostname: System.get_env("PGHOST"),
+	database: System.get_env("PGDATABASE"),
 	show_sensitive_data_on_connection_error: true,
 	pool_size: 10,
   socket_options: [:inet6]
@@ -13,7 +13,7 @@ config :ping, PingWeb.Endpoint,
   http: [ip: {127, 0, 0, 1}, port: 4000],
   debug_errors: true,
   code_reloader: true,
-  check_origin: false,
+  check_origin: true,
   watchers: [],
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
